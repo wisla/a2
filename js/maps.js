02ae26be8d5bbe852f7ebb2d239c2f610fcd915b
wisla <andrzej.wislowski@combeenut.com>
2016-02-22 09:37:00 +0100

    koniec

--- js/maps.js
+++ js/maps.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($) {
     var $marker_url = (is_internetExplorer11) ? 'img/marker.png' : 'img/marker.png';
 
     //define the basic color of your map, plus a value for saturation and brightness
-    var $main_color = '#2d313f',
+    var $main_color = '#c5cad1',
         $saturation = -20,
         $brightness = 5;
 
@@ -224,27 +224,4 @@ jQuery(document).ready(function($) {
         visible: true,
         icon: $marker_url,
     });
-
-    //add custom buttons for the zoom-in/zoom-out on the map
-    function CustomZoomControl(controlDiv, map) {
-        //grap the zoom elements from the DOM and insert them in the map
-        var controlUIzoomIn = document.getElementById('cd-zoom-in'),
-            controlUIzoomOut = document.getElementById('cd-zoom-out');
-        controlDiv.appendChild(controlUIzoomIn);
-        controlDiv.appendChild(controlUIzoomOut);
-
-        // Setup the click event listeners and zoom-in or out according to the clicked element
-        google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-            map.setZoom(map.getZoom() + 1)
-        });
-        google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-            map.setZoom(map.getZoom() - 1)
-        });
-    }
-
-    var zoomControlDiv = document.createElement('div');
-    var zoomControl = new CustomZoomControl(zoomControlDiv, map);
-
-    //insert the zoom div on the top left of the map
-    map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 });
